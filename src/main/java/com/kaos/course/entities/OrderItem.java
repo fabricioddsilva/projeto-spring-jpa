b4ba2f343a40502754e95332616e75c7df1ba948
Fabricio Dias <fabriciodsilva3404@gmail.com>
2024-08-10 14:50:25 -0300

    Associação de muitos para muitos entre Product e OrderItem

--- src/main/java/com/kaos/course/entities/OrderItem.java
+++ src/main/java/com/kaos/course/entities/OrderItem.java
@@ -37,7 +37,7 @@ public class OrderItem implements Serializable {
     public Order getOrder(){
         return id.getOrder();
     }
-    
+
     public void setOrder(Order order){
         id.setOrder(order);
     }
